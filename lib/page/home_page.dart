ea0b82bac890f1fc3a18c5cc62e295acb5d5c4f8
Faik-irkham <faikirkham@gmail.com>
2025-07-15 04:34:22 +0700

    update

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,3 +1,8 @@
+import 'package:ebook_online/models/book_model.dart';
+import 'package:ebook_online/widgets/navbar.dart';
+import 'package:ebook_online/widgets/search_and_profile.dart';
+import 'package:ebook_online/widgets/section_book.dart';
+import 'package:ebook_online/widgets/tabs.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +13,74 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedTab = 0;
+  int _selectedNavItem = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('EBOOK')));
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color.fromARGB(255, 255, 239, 159), Colors.white],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.0, 0.5],
+                ),
+              ),
+              child: ListView(
+                padding: const EdgeInsets.fromLTRB(20, 50, 20, 120),
+                children: [
+                  const SearchAndProfile(),
+                  const SizedBox(height: 30),
+                  EbookAudiobookTabs(
+                    selectedTab: _selectedTab,
+                    onTabChanged: (index) {
+                      setState(() {
+                        _selectedTab = index;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 30),
+                  BookSection(
+                    title: 'Buku Populer',
+                    books: popularBooks,
+                    bookHeight: 220,
+                  ),
+                  const SizedBox(height: 30),
+                  BookSection(
+                    title: 'Rekomendasi Kami',
+                    books: springBooks,
+                    bookHeight: 180,
+                  ),
+                  const SizedBox(height: 30),
+                  BookSection(
+                    title: 'Paling Laris',
+                    books: topSellingBooks,
+                    bookHeight: 180,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            right: 20,
+            child: CustomBottomNavBar(
+              selectedIndex: _selectedNavItem,
+              onItemTapped: (index) {
+                setState(() {
+                  _selectedNavItem = index;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
